4fcb17395b61b80336fc0b90eaf106585f29c3ed
930521 <rlaghlwlsx@gmail.com>
2022-03-07 17:48:08 +0900

    db u_info 추가

--- 김회진.sql
+++ 김회진.sql
@@ -17,7 +17,8 @@ CREATE TABLE trade_user(
     u_addr_detail VARCHAR(50) NOT NULL,
     u_phone VARCHAR(100) NOT NULL,
     u_visit_date TIMESTAMP NOT NULL DEFAULT now(),
-    u_withdraw char(1) DEFAULT 'n'
+    u_withdraw char(1) DEFAULT 'n',
+    u_info char(1) DEFAULT 'y'
 );
 
 
@@ -31,6 +32,8 @@ CREATE TABLE user_auth(
     REFERENCES trade_user(u_id)
 );
 
+update trade_user set u_pw = '123qwe!@#' where uno = 1;
+
 INSERT INTO user_auth(u_id,u_auth) VALUES('qwe@qwe','ROLE_MASTER');
 
 DESC trade_user;
@@ -43,6 +46,7 @@ SELECT * FROM user_auth;
 
 commit;
 
+UPDATE trade_user SET u_withdraw = 'n';
 
 
 
@@ -65,7 +69,23 @@ CREATE TABLE qna_tbl(
     filepath VARCHAR(300)
 );
 
+-- board 생성 및 데이터 삽입 쿼리문
+CREATE TABLE IF NOT EXISTS board(
+	bno INT PRIMARY KEY AUTO_INCREMENT,	-- 번호
+    title VARCHAR(50) NOT NULL,			-- 제목
+    content VARCHAR(50) NOT NULL,		-- 내용
+    writer VARCHAR(50) NOT NULL,		-- 작성자
+    regdate TIMESTAMP DEFAULT NOW(),	-- 등록일자
+    updateDate TIMESTAMP DEFAULT NOW(),	-- 수정일자
+    fileName varchar(500) default null, -- 파일이름
+	filePath varchar(500) default null 	-- 파일경로
+);
 
+insert into board(title,content,writer) values('테스트 제목1','테스트 내용1','최기근');	
+
+-- board 생성 및 데이터 삽입 쿼리문 끝
+
+commit;
 
 
 
